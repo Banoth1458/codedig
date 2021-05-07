942fe56fa89ea928426a65069b8a1d097fa34c47
Banoth <saitom1434@gmail.com>
2021-05-08 03:41:01 +0530

    limit_memory_to_process

---
+++ limit_memory_to_process.c
@@ -0,0 +1,29 @@
+/*
+
+This program is intended to experiment with limiting the resources for this program using kernel's systemd utility
+
+Run without limiting:
+gcc limit_memory_to_process.c -o limit_memory_to_process 
+./limit_memory_to_process
+
+Run with limiting memory (in this example requesting for 1M but limiting the program to 1K:
+gcc limit_memory_to_process.c -o limit_memory_to_process
+systemd-run --scope -p MemoryLimit=1K ./limit_memory_to_process
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void main(){
+//request 1MB of memory 
+int *ptr = (int*)malloc(1024*1024);
+int i;
+//Accessig the allocated memory
+for(i=0;i<(1024*1024/4);i++){
+ptr[i] = 1;
+}
+//sleep used just to monitor memory statics from htop
+sleep(5);
+}
